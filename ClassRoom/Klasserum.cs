59355d365a638dbfef072e3fd6b36f33290434fe
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2018-02-04 15:30:14 +0100

    Updates

---
+++ ClassRoom/Klasserum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Klasserum
+    {
+        public string KlasseNavn { get; set; }
+
+        public List<Studerende> Klasseliste { get; set; }
+
+        public DateTime SemesterStart { get; set; }
+
+        public Klasserum()
+        { 
+        }
+    }
+}
--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,25 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            Klasserum klasserum = new Klasserum();
+
+            klasserum.KlasseNavn = "3F";
+            klasserum.SemesterStart = new DateTime(2018,1,29);
+            klasserum.Klasseliste = new List<Studerende>();
+            
+            Studerende s1 = new Studerende("Peter",10,30);
+            Studerende s2 = new Studerende("David",05,10);
+            Studerende s3 = new Studerende("Lasse",02,18);
+
+            klasserum.Klasseliste.Add(s1);
+            klasserum.Klasseliste.Add(s2);
+            klasserum.Klasseliste.Add(s3);
+            
+
+            foreach (var element in klasserum.Klasseliste)
+            {
+                Console.WriteLine(element);
+            }
         }
     }
 }
